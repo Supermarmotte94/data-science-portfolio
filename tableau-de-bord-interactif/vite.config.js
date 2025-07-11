26b7785c34378fb69eded35b5940c69ca9554e7e
fpieraly <95142335+Supermarmotte94@users.noreply.github.com>
2025-07-11 16:32:09 +0300

    Update vite.config.js

--- tableau-de-bord-interactif/vite.config.js
+++ tableau-de-bord-interactif/vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react( ),tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/data-science-portfolio/tableau-de-bord-interactif/', // CETTE LIGNE EST CRUCIALE
 })
